657b1bc9617e576b008faf6b0f87df416bbdf102
Felix Lange <fjl@twurst.com>
2014-01-10 19:59:07 +0100

    remove unused lager dependency

--- src/jsonpath.erl
+++ src/jsonpath.erl
@@ -26,8 +26,6 @@
 -export([search/2, replace/3, add/3]).
 -export([parse_path/1]).
 
--include("jsonpath.hrl").
-
 search(Path, Data) when is_binary(Data) ->
 	search(Path, jiffy:decode(Data));
 search(Path, Data) ->
@@ -186,10 +184,8 @@ add_tuples( [SearchHead | SearchTail], AddData, [FirstTuple | RestTuple], Accum 
 replace_data([SearchHead|SearchTail], Replace, Structure) ->
 	case Structure of 
 		{TupleList} ->
-			%?DEBUG("tuple list: ~p", [TupleList]),
 			{ replace_tuple_list([SearchHead|SearchTail], Replace, TupleList) };
 		List ->
-			%?DEBUG("looks like a list: ~p", [List]),
 			replace_list([SearchHead|SearchTail], Replace, List)
 	end.
 
@@ -198,33 +194,26 @@ replace_list([SearchHead|SearchTail], Replace, List) ->
 		Index = list_to_integer(binary_to_list(SearchHead)) + 1,
 		case (Index > length(List)) of
 			true ->
-				%?DEBUG("Index out of range ~p for list size ~p", [Index, length(List)]),
 				undefined;
 			false ->
 				replace_list([Index|SearchTail], Replace, List, 1, [])
 		end
 	catch
 		_:_ ->
-			%?DEBUG("This is not an integer: ~p", [SearchHead]),
 			undefined
 	end.
 replace_list([_SearchHead|_SearchTail], _Replace, [], _Count, Accum) ->
-	%?DEBUG("at the end of this list with accum: ~p", [Accum]),
 	lists:reverse(Accum);
 replace_list([SearchHead|SearchTail], Replace, [Head|Tail], Count, Accum) ->
-	%?DEBUG("list: ~p", [Head|Tail]),
 	Data = case SearchHead of 
 		Count ->
-			%?DEBUG("Found index ~p", [Count]),
 			case SearchTail of
 				[] ->
 					Replace;
 				_SearchTail ->
-					%?DEBUG("Not last, so no replacement, but replaceing into: ~p", [Head]),
 					replace_data(SearchTail, Replace, Head)
 			end;
 		_SearchHead ->
-			%?DEBUG("Not index ~p", [Count]),
 			Head
 	end,
 	replace_list([SearchHead|SearchTail], Replace, Tail, Count+1, [Data|Accum]).
@@ -233,47 +222,37 @@ replace_list([SearchHead|SearchTail], Replace, [Head|Tail], Count, Accum) ->
 replace_tuple_list([SearchHead|SearchTail], Replace, TupleList) ->
 	replace_tuple_list([SearchHead|SearchTail], Replace, TupleList, []).
 replace_tuple_list([_SearchHead|_SearchTail], _Replace, [], Accum) ->
-	%?DEBUG("at the end of this tuple list with accum: ~p", [Accum]),
 	lists:reverse(Accum);
 replace_tuple_list([SearchHead|SearchTail], Replace, [Head|Tail], Accum) ->
-	%?DEBUG("tuple: ~p", [Head]),
 	Data = case Head of
 		{SearchHead, Value} ->
-			%?DEBUG("Found match for ~p: ~p", [SearchHead, {SearchHead, Value}]),
 			case SearchTail of 
 				[] ->
 					{SearchHead, Replace};
 				_SearchTail ->
-					%?DEBUG("Not last, so no replacement, but replaceing into : ~p",[Head]),
 					{SearchHead, replace_data(SearchTail, Replace, Value) }
 			end;
 		_Other ->
-			%?DEBUG("No match for ~p: ~p", [SearchHead, Other]),
 			Head
 	end,
-	%?DEBUG("continue processing tail: ~p", [Tail]),
 	replace_tuple_list([SearchHead|SearchTail], Replace, Tail, [Data|Accum]).
 
 search_data([], Data) ->
 	Data;
 search_data([Head|Tail], Data) ->
-	%?DEBUG("Searching for ~p in ~p", [Head,Data]),
 	case Head of
 		<<>> ->
 			search_data(Tail, Data);
 		_Other ->
 			case Data of 
 				{_Tuple} ->
-					%?DEBUG("found tuple: ~p", [Tuple]),
 					search_tuple([Head|Tail], Data);
 				_List ->
-					%?DEBUG("found list: ~p", [List]),
 					search_list([Head|Tail], Data)
 			end
 	end.
 
 search_list([Head|Tail], List) ->
-	%?DEBUG("list search for ~p in ~p",[Head, List]),
 	try 
 		Index = list_to_integer(binary_to_list(Head)) + 1,
 		case (Index > length(List)) of
@@ -284,7 +263,6 @@ search_list([Head|Tail], List) ->
 		end
 	catch
 		_:_ -> 
-			%?DEBUG("that wasn't an integer",[]),
 			undefined
 	end.
 
@@ -292,10 +270,8 @@ search_tuple([Head|Tail], Tuple) ->
 	{TuplePayload} = Tuple,
 	case lists:keyfind(Head, 1, TuplePayload) of
 		false ->
-			%?DEBUG("did not find tuple value for ~p. done.", [Head]),
 			undefined;
 		{Head,Value} ->
-			%?DEBUG("found tuple value for ~p: ~p", [Head,Value]),
 			search_data(Tail, Value)
 	end.
 
